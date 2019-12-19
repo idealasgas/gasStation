6a3c5b33912b116777c242e04739728376c59bb3
Marie Akhramenko <triangletrue@yahoo.com>
2019-12-19 21:04:47 +0300

    UI slightly works

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,9 +1,40 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class MainMenu {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainMenu {
+    @FXML private Label counterOfTime;
+    @FXML private Button closeButton;
+    private int counter;
+
+    public void startSimulation(ActionEvent event) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        counterOfTime.setText("Прошло времени с момента запуска: " + counter++ + " сек.");
+                    }
+                });
+            }
+        }, 0, 1000);
+
+    }
+
+    public void stopSimulation() {
+
+    }
+
+    public void exit(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
